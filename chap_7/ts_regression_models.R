4f3b740686d0ff47030dc48275450135b020dbee
Joaquin Romero Flores <jromerof30@gmail.com>
2024-03-08 11:34:48 -0700

    checking sporious correlation, prediction intervals

--- chap_7/ts_regression_models.R
+++ chap_7/ts_regression_models.R
@@ -156,17 +156,105 @@ fc_beer %>%
 
 # Scenario based forecasting
 
+fit_consBest <- us_change %>%
+  model(
+    lm = TSLM(Consumption ~ Income + Savings + Unemployment)
+  )
 
+future_scenarios <- scenarios(
+  Increase = new_data(us_change, 4) %>%
+    mutate(Income=1, Savings=0.5, Unemployment=0),
+  Decrease=new_data(us_change, 4) %>%
+    mutate(Income=-1, Savings=-0.5, Unemployment=0),
+  names_to = "Scenarios")
 
+fc <- forecast(fit_consBest, new_data = future_scenarios)
+us_change %>%
+  autoplot(Consumption) +
+  autolayer(fc) +
+  labs(title="US cosumption", y = "% change")
 
+# Building a Predictive Regression Model
 
+# Prediction Intervals
 
+fit_cons <- us_change %>%
+  model(TSLM(Consumption ~ Income))
 
+new_cons <- scenarios(
+  "Average increase" = new_data(us_change, 4) %>%
+    mutate(Income = mean(us_change$Income)),
+  "Extreme increase" = new_data(us_change, 4) %>%
+    mutate(Income = 12),
+  names_to = "Scenario"
+)
 
+fcast <- forecast(fit_cons, new_cons)
 
+us_change %>%
+  autoplot(Consumption) +
+  autolayer(fcast) +
+  labs(title = "US consumption", y = "% change")
 
+# Forecasting a Non-Linear Trend
+# Boston Marathon Winning
 
+boston_men <- boston_marathon %>%
+  filter(Year >= 1924) %>%
+  filter(Event == "Men's open division") %>%
+  mutate(Minutes = as.numeric(Time)/60)
 
+fit_trends <- boston_men %>%
+  model(
+    linear = TSML(Minutes ~ trend()),
+    exponential = TSML(log(Minutes) ~ trend()),
+    piecewise = TSML(Minutes ~ trend(knots = c(1950, 1980)))
+  )
+
+fc_trends <- fit_trends %>% forecast(h = 10)
+
+boston_men %>%
+  autoplot(Minutes) +
+  geom_line(data = fitted(fit_trends),
+            aes(y = .fitted, colour = .model)) +
+  autolayer(fc_trends, alpha = 0.5, level = 95) +
+  labs(y = "Minutes",
+       title = "Boston marathon winning times")
+
+# Alternative
+
+library(fable)
+library(tsibble)
+library(dplyr)
+library(ggplot2)
+
+boston_men <- boston_marathon %>%
+  filter(Year >= 1924, Event == "Men's open division") %>%
+  mutate(Minutes = as.numeric(Time) / 60) %>%
+  as_tsibble(index = Year)
+
+# Creating indicator variables for piecewise regression
+boston_men <- boston_men %>%
+  mutate(after_1950 = pmax(Year - 1950, 0),
+         after_1980 = pmax(Year - 1980, 0))
+
+fit_trends <- boston_men %>%
+  model(
+    linear = TSLM(Minutes ~ trend()),
+    exponential = TSLM(log(Minutes) ~ trend()),
+    piecewise = TSLM(Minutes ~ trend() + after_1950 + after_1980)
+  )
+
+fc_trends <- fit_trends %>% forecast(h = 10)
+
+# Plotting
+boston_men %>%
+  autoplot(Minutes) +
+  geom_line(data = fitted(fit_trends),
+            aes(y = .fitted, colour = .model)) +
+  autolayer(fc_trends, alpha = 0.5) +
+  labs(y = "Minutes",
+       title = "Boston Marathon Winning Times")
 
 
 
